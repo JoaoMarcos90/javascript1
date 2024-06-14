a1cf29770ca81e4aeea9268b4786235d4865ce1f
joao969 <154928430+JoaoMarcos90@users.noreply.github.com>
2024-06-14 16:22:21 -0300

    9

--- estrutura.js
+++ estrutura.js
@@ -1,1 +1,33 @@
 
+const sr= ScrollReveal({
+    distance :'10%',
+    duration: 2000,
+    delay: 5,
+    reset:true
+  });
+  
+  
+  sr.reveal('.linha1',{delay:200, origin:'top'});
+  sr.reveal('.img3',{delay:100, origin:'top'});
+  sr.reveal('.botao',{delay:100, origin:'top'});
+  sr.reveal('.h4',{delay:-200, origin:'top'});
+
+/*---------------------------------TOPO -----------------------------------------*/
+
+  $(document).ready(function() {
+	$('#subir').click(function(){
+		$('html, body').animate({scrollTop:0}, 'slow');
+		return false;
+	});
+});
+
+
+/* ------- FUNCAO ANCORA ----------------------------------------*/
+
+var $doc = $('html, body');
+$('.galeria').click(function() {
+  $doc.animate({
+      scrollTop: $( $.attr(this,'href')).offset().top
+  },500);
+  return false;
+});
